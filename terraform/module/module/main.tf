137686a2618a919f6e9e44e7032f98c21d8d1473
tejas wadaskar <tejaswadaskar@tejass-MacBook-Air.local>
2026-02-11 11:34:22 +0530

    t3

--- terraform/module/module/main.tf
+++ terraform/module/module/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "./module/main/EC2"
+  source = "/home/ubuntu/deleteafterpractice/terraform/module/main/EC2"
   webserver_ami =  var.webserver_ami 
   webserver_instance_type = "t3.micro"
   webserver_keyname = "linux2"
@@ -8,10 +8,10 @@ module "ec2" {
   webserver_subnetA = module.vpc.subnet_id
 
 
-} 
+}
 
 module "vpc" {
-  source =  "./module/main/VPC"
+  source =  "/home/ubuntu/deleteafterpractice/terraform/module/main/VPC"
   vpc_cidr = "172.30.0.0/16"
   vpc_subnetA = "172.30.128.0/20"
   public_ip = true
