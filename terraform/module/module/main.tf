a57ef7c1217f1a78a2c65d61991c03e378e4da40
tejas wadaskar <tejaswadaskar@tejass-MacBook-Air.local>
2026-02-11 10:47:43 +0530

    t2

---
+++ terraform/module/module/main.tf
@@ -0,0 +1,20 @@
+module "ec2" {
+  source = "/Users/tejaswadaskar/deleteafterpractice/terraform/module/main/EC2"
+  webserver_ami =  var.webserver_ami 
+  webserver_instance_type = "t3.micro"
+  webserver_keyname = "linux2"
+  webserver_disable_api_termination = false
+  webserver_sg = module.vpc.webserversg
+  webserver_subnetA = module.vpc.subnet_id
+
+
+} 
+
+module "vpc" {
+  source =  "/Users/tejaswadaskar/deleteafterpractice/terraform/module/main/VPC"
+  vpc_cidr = "172.30.0.0/16"
+  vpc_subnetA = "172.30.128.0/20"
+  public_ip = true
+  az = "us-east-1a"
+
+}
